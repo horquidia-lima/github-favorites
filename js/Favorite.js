47f6835acd2f71a07ebadf2b9785de41dab61e14
horquidia-lima <horquidia.a.lima@gmail.com>
2023-11-24 22:36:55 -0300

    Imutabilidade para trabalhar com os dados da aplicação

--- js/Favorite.js
+++ js/Favorite.js
@@ -23,6 +23,12 @@ export class Favorites {
       }
     ]
   }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(entry => 
+      entry.login !== user.login)
+      console.log(filteredEntries)
+  }
 }
 
 //classe que vai criar a visualizacao e ventos do HTML
@@ -47,6 +53,13 @@ export class FavoritesView extends Favorites {
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('tem certeza que deseja deletar essa linha?')
+        if(isOk){
+          this.delete(user)
+        }
+      }
 
       this.tbody.append(row)
     })
